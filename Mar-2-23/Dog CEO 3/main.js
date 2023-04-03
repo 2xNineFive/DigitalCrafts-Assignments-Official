3472edc0d9828a80a74a3a8c987a392b48e8702b
Frankie Molina <officialgoldminded@gmail.com>
2023-04-03 14:14:29 -0700

    no changes

--- Mar-2-23/Dog CEO 3/main.js
+++ Mar-2-23/Dog CEO 3/main.js
@@ -2,9 +2,11 @@ const getAllBreeds = async () => {
     const x = await fetch("https://dog.ceo/api/breeds/list/all")
     const response = await x.json();
     const data = response.message;
-    // console.log(data);
+    console.log(data);
     return data;
 }; 
+
+// getAllBreeds();
 
 async function getBreedAndSubBreedCount() {
     const data = await getAllBreeds();
@@ -14,32 +16,20 @@ async function getBreedAndSubBreedCount() {
     const breeds = Object.keys(data);
     // console.log(breeds);
 
-    // // hard-coding answer
-    // const key0 = breeds[0];
-    // // console.log(aff);
-    // let value0 = data.affenpinscher;
-    // // console.log(newAff);
-    // if (value0 < 1) {
-    //     value0 = 1;
-    // }
-    // const answer0 = `${key0} - ${value0}`;
-    // // console.log(answer0);
 
+    let emptyArray = []
 
+    breeds.forEach(breed => {
+        // console.log(data[breed]);
+        // console.log(data[breed].length);
+        if(data[breed].length < 1) {
+            emptyArray.push(`${breed} - 1`);
+        } else {
+            emptyArray.push(`${breed} - ${data[breed].length}`);
+        }
+    });
 
-    // let emptyArray = []
-
-    // breeds.forEach(breed => {
-    //     // console.log(data[breed]);
-    //     // console.log(data[breed].length);
-    //     if(data[breed].length < 1) {
-    //         emptyArray.push(`${breed} - 1`);
-    //     } else {
-    //         emptyArray.push(`${breed} - ${data[breed].length}`);
-    //     }
-    // });
-
-// console.log(emptyArray);
+console.log(emptyArray);
 }
 
 getBreedAndSubBreedCount();
